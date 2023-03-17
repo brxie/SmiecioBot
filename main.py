4546ff8d1abf3655597e97d2d887d64e1ed669f2
brxie <morpher89@gmail.com>
2023-03-17 19:10:18 +0100

    Debug mode

--- main.py
+++ main.py
@@ -20,14 +20,15 @@ from selenium.webdriver.support.ui import WebDriverWait
 username = getenv("FB_EMAIL")
 password = getenv("FB_PASSWORD")
 threadId = getenv("THREAD_ID")
+debug = getenv("DEBUG", 'false').lower() in ('true', '1', 't')
+visible = getenv("UI", 'false').lower() in ('true', '1', 't')
 trigger_message_time = getenv("TRIGGER_TIME", "16:00")
-DRY_RUN = False
 
 ICS_URL = "https://p71-caldav.icloud.com/published/2/MTg0MjE0MzQ0MzE4NDIxNPUuBwTTG2rEEZaB3IqTt-sjB3X-WT2A4qKi9Upx_iZEhgNqVDvPAFRp_3Py3PMMOlEMlZphzr4aBaBde3jzqm0"
 COOKIE_BUTTON_XPATH = "//button[@ data-cookiebanner='accept_only_essential_button']"
 MESSAGE_TEXT_XPATH = "//div[@aria-label='Wiadomość']"
 
-display = Display(visible=False)
+display = Display(visible=visible)
 display.start()
 
 chrome_driver = webdriver.Chrome()
@@ -119,19 +120,17 @@ def trigger_message(ics):
     events = lookup_events(cal)
     if events:
         fb_msg = ["Tutaj SmiecioBot ♻"]
-        fb_msg.append("Przygotuj na jutro (%s): " % (
+        fb_msg.append("Wystaw na jutro ☀️ (%s): " % (
             weekDaysPL[events[0]["dtstart"].isoweekday()]
         ))
         for i, evt in enumerate(events):
-            evt_text = "☑ %s" % (evt["summary"])
+            evt_text = "➡ %s" % (evt["summary"])
             if evt["description"] is not None:
                 evt_text += " (%s)" % evt["description"]
             fb_msg.append(evt_text)
 
-        fb_msg.append("Dziękuję")
         logging.info("sending message: '%s'" % " ".join(str(x) for x in fb_msg))
-        if not DRY_RUN:
-            send_message(fb_msg)
+        send_message(fb_msg)
     else:
         logging.info("no events today")
 
@@ -146,9 +145,11 @@ if __name__ == "__main__":
 
     login()
 
+    if debug: trigger_message(ics)
+
     schedule.every().day.at(trigger_message_time).do(trigger_message, ics)
     logging.info("SmiecioBot started!")
-    trigger_message(ics) # debug
+
     while True:
         schedule.run_pending()
         sleep(60)
